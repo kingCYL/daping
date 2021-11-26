ee18fe82fd126b5724e60eb754770e981257d7cd
红衣 <498349470@qq.com>
2021-11-26 17:28:00 +0800

    temp:自动补仓
    temp:自动补仓

--- vue.config.js
+++ vue.config.js
@@ -30,4 +30,38 @@ module.exports = {
       },
     })
   },
+  
+   function sum(lastPrice = 1, NowPrice = 1, cost, n = 1) {
+        let lastAveragePrice = cost / n
+
+        // 跌幅0.4
+        if (NowPrice <= 0.6 * lastPrice) {
+          lastPrice = NowPrice
+          cost = cost * 2
+          n = cost / NowPrice
+        }
+
+        let nowAveragePrice = cost / n
+        let isTarget = nowAveragePrice - lastAveragePrice === 3 ? true : false
+
+        return {
+          lastPrice,
+          NowPrice,
+          n,
+          isTarget,
+          cost,
+        }
+      }
+
+      function buy() {
+        let { lastPrice, NowPrice, n, isTarget, cost } = this.sum(1, 1, 10, 1)
+
+        while (!isTarget) {
+          buy(lastPrice, NowPrice, cost, n)
+        }
+
+        console.log('n---', n)
+      }
+
+      this.buy()
 }
